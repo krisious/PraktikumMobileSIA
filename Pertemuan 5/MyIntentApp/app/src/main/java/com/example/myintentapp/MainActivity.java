f58f959e29e5e3aa660622ce5bf377d6dadede8b
krisious <fransdwikrisdito20@gmail.com>
2023-09-19 09:26:06 +0700

    Latihan Intent Lanjutan

--- Pertemuan 5/MyIntentApp/app/src/main/java/com/example/myintentapp/MainActivity.java
+++ Pertemuan 5/MyIntentApp/app/src/main/java/com/example/myintentapp/MainActivity.java
@@ -7,11 +7,17 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btnMoveActivity;
     private Button btnMoveWithDataActivity;
     private Button btnDialPhone;
+    private Button btnMoveWithObject;
+    private Button btnMoveForResult;
+    private TextView tvResult;
+    private int REQUEST_CODE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnDialPhone = (Button)findViewById(R.id.btn_dial_number);
         btnDialPhone.setOnClickListener(this);
+
+        btnMoveWithObject = (Button)findViewById(R.id.btn_move_activity_object);
+        btnMoveWithObject.setOnClickListener(this);
+
+        btnMoveForResult = (Button)findViewById(R.id.btn_move_for_result);
+        btnMoveForResult.setOnClickListener(this);
+        tvResult = (TextView)findViewById(R.id.tv_result);
     }
 
     @Override
@@ -42,6 +55,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String phoneNumber = "081210841382";
             Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
             startActivity(dialPhoneIntent);
+        } else if (id == R.id.btn_move_activity_object){
+            Person mPerson = new Person();
+            mPerson.setName("Dwi Krisdiyanto");
+            mPerson.setAge(20);
+            mPerson.setEmail("fransdwikrisdito20@gmail.com");
+            mPerson.setCity("Kota Bekasi");
+
+            Intent moveWithObjectIntent = new Intent(MainActivity.this, MoveWithObjectActivity.class);
+
+            moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, mPerson);
+            startActivity(moveWithObjectIntent);
+        } else if(id == R.id.btn_move_for_result){
+            Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultActivity.class);
+            startActivityForResult(moveForResultIntent, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == MoveForResultActivity.RESULT_CODE) {
+                int selectedValue = data.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0);
+                tvResult.setText("Hasil : "+selectedValue);
+
+            }
         }
     }
 }
