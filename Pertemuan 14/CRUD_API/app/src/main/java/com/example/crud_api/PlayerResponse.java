fd148edcacbb606274ef4e784665548b64ded560
krisious <fransdwikrisdito20@gmail.com>
2023-11-21 09:27:47 +0700

    Pertemuan14

---
+++ Pertemuan 14/CRUD_API/app/src/main/java/com/example/crud_api/PlayerResponse.java
@@ -0,0 +1,14 @@
+package com.example.crud_api;
+
+import java.util.List;
+
+public class PlayerResponse<T> {
+    List<T> result;
+    public List<T> getResult() {
+        return result;
+    }
+    public void setResult(List<T> result) {
+        this.result = result;
+    }
+
+}
